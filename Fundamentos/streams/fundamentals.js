267273dd982f15d4ea1b77c350f7b265230c0caa
Jhonata Souza <einasota@gmail.com>
2024-02-27 19:00:12 -0300

    end of 3.3 class

--- Fundamentos/streams/fundamentals.js
+++ Fundamentos/streams/fundamentals.js
@@ -1,7 +1,7 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import {Readable} from "node:stream"
+import { Readable, Writable, Transform } from "node:stream"
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -9,7 +9,7 @@ class OneToHundredStream extends Readable {
         const i = this.index++
 
         setTimeout(() => {
-            if(i > 100) {
+            if (i > 100) {
                 this.push(null)
             } else {
                 const buf = Buffer.from(String(i))
@@ -19,4 +19,22 @@ class OneToHundredStream extends Readable {
     }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunck, encoding, callback) {
+        console.log(Number(chunck.toString()) * 10)
+        callback()
+    }
+}
+
+
+
+new OneToHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
