6870c010058e26762b80a4cbc7f09ec1960982ba
Jhonata Souza <einasota@gmail.com>
2024-02-27 18:14:43 -0300

    end of 2.3 class

--- Fundamentos/src/server.js
+++ Fundamentos/src/server.js
@@ -18,10 +18,10 @@ const server = http.createServer((req, res) => {
             name: 'John Doe',
             email: 'johndoe@example.com',
         })
-        return res.end('Criação de Usuário')
+        return res.writeHead(201).end()
     }
 
-    return res.end('Hello World!')
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
